1da0f2f157db29f7578b50b5659f894905514258
Berdnikov Alexey <berdnikov_ay@taximaxim.ru>
2017-04-13 11:30:32 +0500

    fixed

--- init.php
+++ init.php
@@ -1,6 +1,5 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-angular-named-route')
     ->usePackage('larakit/sf-angular')
-    ->ngModule('ngNamedRoute')
     ->setSourceDir('public')
     ->jsPackage('angular-named-route.js');
